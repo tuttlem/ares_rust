d837b1347e68b31102e3b5c3c948885dd9983b41
Michael Tuttle <tuttlem@gmail.com>
2025-10-18 09:44:03 +1000

    tests

--- target/debug/deps/vfs_tests-2b2502f6b33411e8.d
+++
@@ -1,5 +0,0 @@
-/home/michael/src/os/ares_rust/target/debug/deps/vfs_tests-2b2502f6b33411e8.d: crates/ares-core/tests/vfs_tests.rs
-
-/home/michael/src/os/ares_rust/target/debug/deps/vfs_tests-2b2502f6b33411e8: crates/ares-core/tests/vfs_tests.rs
-
-crates/ares-core/tests/vfs_tests.rs:
